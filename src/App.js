387ac9531e9d63bf1b6dd69ed4d82455f0ffc8ef
Charon <rinrancharon.devops@gmail.com>
2024-01-25 12:47:13 +0800

    Add react-responsive package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
+import { useMediaQuery } from "react-responsive"
 import Game from "./game";
 
 export default function App(){
+    const isMobileDevice = useMediaQuery({
+        query: "(min-device-width: 480px)",
+    });
+
+    const isTabletDevice = useMediaQuery({
+        query: "(min-device-width: 768px)",
+    });
+
+    const isLaptop = useMediaQuery({
+        query: "(min-device-width: 1024px)",
+    });
+
+    const isDesktop = useMediaQuery({
+        query: "(min-device-width: 1200px)",
+    });
+
+    const isBigScreen = useMediaQuery({
+        query: "(min-device-width: 1201px )",
+    });
+
+
+
     return (
         <Game /> );
 }
