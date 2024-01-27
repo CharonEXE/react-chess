e618122c46ed2f18897c8c80f2e7a42fc18e4faa
Charon <rinrancharon.devops@gmail.com>
2024-01-27 18:47:05 +0800

    Add in button mockup;

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import { useState, useEffect, useMemo, useCallback, forwardRef } from "react";
-import { useMediaQuery } from "react-responsive"
+// import { useState, useEffect, useMemo, useCallback, forwardRef } from "react";
+// import { useMediaQuery } from "react-responsive"
 import './App.css'
 import Game from "./game";
 
 export default function App(){
+
+    // TODO Add different component for different screensize based on width pixel
+    // TODO https://blog.logrocket.com/using-react-responsive-to-implement-responsive-design/
+    
     // const [chessboardSize, setChessboardSize] = useState(undefined);
     // const [selectedBoard, setSelectedBoard] = useState("PlayVsRandom");
   
@@ -39,12 +43,34 @@ export default function App(){
     // });
 
 
+    // TODO with Notes 
+    // Three main custom components
+    // 1) User Interface features (Main Menu... bar(?), history, forward, backward)
+    // 2) Game Interface (which done developed, pending improvement)
+    // 3) Stockfish Interface (eval, best move, bot...) !!!
+
+    // <MainMenuBar>
+
+
 
     return (
         <div className="container">
             <div className="split-container">
                 <div className="split-row">
                     <div className="split-column left-pane">
+                        <div className='empty-container'></div>
+                        <div className='log-container'>
+                            <div className='main-log'></div>
+                        </div>
+                        <div className='main-button-container'>
+                            <button className='main-button'>Reset Game</button>
+                            <button className='main-button'>New VS Bot Game</button>
+                        </div>
+                        <div className='main-button-container'>
+                            <button className='main-button'>Save Game</button>
+                            <button className='main-button'>History</button>
+                        </div>
+
                         <div id="evaluation" className="ai"></div>
                     </div>
                     <div className="split-column right-pane">
